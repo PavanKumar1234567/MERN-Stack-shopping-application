9546816157c6c70f18258d4476392cfbcbfce975
PavanKumar1234567 <97537305+PavanKumar1234567@users.noreply.github.com>
2024-01-08 07:23:44 +0000

    final

--- client/src/component/Header/Header.js
+++ client/src/component/Header/Header.js
@@ -33,7 +33,7 @@ const Navbar = ({ auth: { isAuthenticated }, logout }) => {
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <Link className="navbar-brand" to='/'>Shopping App</Link>
+            <Link className="navbar-brand" to='/'>PavanMart</Link>
 
             <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
