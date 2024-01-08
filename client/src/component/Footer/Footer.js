93d76d93861d211cb108ec835fdead3c8e89e943
PavanKumar1234567 <97537305+PavanKumar1234567@users.noreply.github.com>
2024-01-08 05:49:32 +0000

    final

--- client/src/component/Footer/Footer.js
+++ client/src/component/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <div style={{"marginTop":"70px"}}>
             <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-bottom">
             <div className="container">
-                <Link className="navbar-brand mr-auto ml-auto" >Shopping App &copy; 2021</Link>
+                <Link className="navbar-brand mr-auto ml-auto" >Pavan's Shopping App &copy; 2024</Link>
             </div>
             </nav>
             </div>
